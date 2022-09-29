930848d0923fa9d50b69d09c2eee98ed394ab4e5
Ivan Shirshikov <ivansh@stud.ntnu.no>
2022-09-29 18:19:47 +0200

    django migrations

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,9 @@
 from email.quoprimime import body_check
 from django.db import models
-
+#make migrations
 class Article(models.Model):
     title = models.CharField(max_length=128)
     slug = models.SlugField()
     body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True);
+    date = models.DateTimeField(auto_now_add=True)
+
