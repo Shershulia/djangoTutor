ac7a188a0718fa0db7731aebf3e761e7699eab8e
Ivan Shirshikov <ivansh@stud.ntnu.no>
2022-09-29 18:10:22 +0200

    django article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
+from email.quoprimime import body_check
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=128)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True);
