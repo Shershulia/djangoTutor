4d7e67b86b7b05420ee2d3cb3818f3875cac84a9
Ivan Shirshikov <ivansh@stud.ntnu.no>
2022-10-06 15:45:52 +0200

    Uploading media in article detail

--- articles/models.py
+++ articles/models.py
@@ -1,11 +1,14 @@
+from email.mime import image
 from email.quoprimime import body_check
 from django.db import models
 #make migrations
+#migrate - to database
 class Article(models.Model):
     title = models.CharField(max_length=128)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default="default.png",blank=True)
 
     #display the query
     def __str__(self):
--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,7 +1,11 @@
+import imp
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path , include
 from djangonautic import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static 
+from django.conf import settings    
 
 
 urlpatterns = [
@@ -12,3 +16,4 @@ urlpatterns = [
 ]
 
 urlpatterns+= staticfiles_urlpatterns()
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
