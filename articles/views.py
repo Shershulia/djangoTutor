13c98697eb6a25298f9ed84aa5c42f49dea0854f
Ivan Shirshikov <ivansh@stud.ntnu.no>
2022-10-06 15:20:13 +0200

    Articles detail

--- articles/views.py
+++ articles/views.py
@@ -4,9 +4,10 @@ from .models import Article
 from django.http import HttpResponse
 
 def article_list(request):
-    article = Article.objects.all().order_by("date")
-    return render(request, 'articles/article_list.html',{'articles':article})
+    articles = Article.objects.all().order_by("date")
+    return render(request, 'articles/article_list.html',{'articles':articles})
 
 def article_detail(request,slug):
-    return HttpResponse(slug)
-
+    #return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request,"articles/article_detail.html",{'article':article})
