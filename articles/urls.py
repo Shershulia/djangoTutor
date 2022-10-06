1ee2000ad20a42182f961717c7aea89532ac0fcc
Ivan Shirshikov <ivansh@stud.ntnu.no>
2022-10-06 15:12:41 +0200

    Named urls

--- articles/urls.py
+++ articles/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from articles import views
 
-
+app_name = 'articles'
 urlpatterns = [
-    path('',views.article_list),
-    path('<slug:slug>/', views.article_detail),
+    path('',views.article_list,name="list"),
+    path('<slug:slug>/', views.article_detail,name = "detail"),
 ]
