1910f573380702560e43099012db2169c32aa25b
Ivan Shirshikov <ivansh@stud.ntnu.no>
2022-10-13 17:03:53 +0200

    Form for create article

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,9 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by("date")
@@ -15,5 +16,13 @@ def article_detail(request,slug):
 
 @login_required(login_url="/accounts/login/") #protection for login 
 def article_create(request):
-    return render(request,"articles/create.html")
+    if (request.method == "POST"):
+        form=forms.CreateArticle(request.POST,request.FILES)
+        if form.is_valid():
+            #save to the database
+            return redirect("articles:list")
+    else:
+        form=forms.CreateArticle()
+
+    return render(request,"articles/create.html", {"form":form}) #3rd parameter - sending to html
 
